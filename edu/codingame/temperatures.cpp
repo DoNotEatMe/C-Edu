b88517cef858c22f4ac5c5e096164d06a749070d
DoNotEatMe <donoteatme1@gmail.com>
2021-12-26 23:57:02 +0300

    Update temperatures.cpp

--- edu/codingame/temperatures.cpp
+++ edu/codingame/temperatures.cpp
@@ -8,21 +8,23 @@ using namespace std;
 //  Write a program that prints the temperature closest to 0 among input data.
 //  If two numbers are equally close to zero, positive integer has to be considered closest to zero
 //  (for instance, if the temperatures are -5 and 5, then display 5).
+// Display 0 (zero) if no temperatures are provided. Otherwise, display the temperature closest to 0.
 
 int *input(const int &lenght, int choose);
 int *testcase(const int &lenght, int choose);
 void ptintArray(int *arr, const int &lenght);
-void nearest( int *arr, const int &lenght)
-
+void nearest(int *arr, const int &lenght);
 
 int main()
 {
     srand(time(0));
-    int lenght = rand() % 10;
-    for (int i = 0; i < 3; i++)
+    int lenght = 3 + rand() % 10;
+    cout << endl;
+    for (int i = 1; i <= 3; i++)
     {
-        int *arr = testcase(lenght, i + 1);
+        int *arr = testcase(lenght, i); // делаем указатель в цикле. Как его разрушить?
         ptintArray(arr, lenght);
+        nearest(arr, lenght);
     }
 }
 
@@ -52,6 +54,7 @@ int *testcase(const int &lenght, int choose)
 int *input(const int &lenght, int choose)
 {
     int *arr = new int[lenght];
+
     for (int i = 0; i < lenght; i++)
     {
         choose == 1 ? arr[i] = -30 + rand() % 60 : false;
@@ -68,5 +71,23 @@ void ptintArray(int *arr, const int &lenght)
     {
         cout << arr[i] << " ";
     }
-    cout << endl << endl;
+    cout << endl;
+}
+
+void nearest(int *arr, const int &lenght)
+{
+    int result(0), max(INT_MAX), min(INT_MIN);
+
+    for (int i = 0; i < lenght; i++)
+    {
+        arr[i] > 0 ? (arr[i] < max ? max = arr[i] : false) : false;
+        arr[i] < 0 ? (arr[i] > min ? min = arr[i] : false) : false;
+    }
+
+    (max == INT_MAX && min == INT_MIN) ? result = 0 : max - (min * -1) <= 0 ? result = max
+                                                                            : result = min;
+
+    cout << "Nearest to zero result is: " << result <<  endl;
+    cout << endl
+         << endl;
 }
