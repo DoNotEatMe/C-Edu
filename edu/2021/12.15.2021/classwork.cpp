ac1bc2ad8aa18afd13ad3001c59d8ba358652cfe
Pin <donoteatme1@gmail.com>
2022-01-26 02:50:18 +0300

    naming

---
+++ edu/2021/12.15.2021/classwork.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <cmath>
+
+using namespace std;
+
+void hello(){
+    int x;
+    cin >> x;
+    if ( x%2 == 0) cout << "yes";
+    if ( x%2 != 0) cout << "no";
+
+}
+
+int main()
+{
+   hello();
+
+}
---
+++ edu/2021/12.29.2021/classwork.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+main()
+{
+    srand(time(0));
+    int lenght = 3 + rand() % 13;
+    vector<int> arr(lenght);
+    int val(0), posInd;
+
+    cout << "insert value to paste: ";
+    cin >> val;
+    cout << "insert index to paste between 0 and " << lenght - 1 << ": ";
+    cin >> posInd;
+
+    for (int i = 0; i < lenght; i++)
+    {
+
+        if (i >= posInd)
+        {
+            arr[i + 1] = -50 + rand() % 100;
+            i == posInd ? arr[i] = val : false;
+        }
+        else
+        {
+            arr[i] = -50 + rand() % 100;
+        }
+    }
+
+    for (int i = 0; i < lenght + 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
